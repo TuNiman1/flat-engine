c79ec7065c6bc65ce07be5c96215d2279cb9f092
TuNiMan1 <tunibendor@gmail.com>
2023-04-06 19:40:11 +0300

    Girrafe Enterance

--- graphics/window/SDLWindow.cpp
+++ graphics/window/SDLWindow.cpp
@@ -2,7 +2,7 @@
 
 #include "../GraphicsAPI.h"
 
-#ifdef FL_GRAPHICS_SDL
+// #ifdef FL_GRAPHICS_SDL
 
 #include "../../glad/include/glad/glad.h"
 #include <SDL2/SDL.h>
@@ -11,12 +11,20 @@
 
 namespace fl
 {
-    SDLWindow::SDLWindow() : Window(INPUTCODES_SDL), m_window(nullptr), m_context(), m_closeRequested(false)
+    SDLWindow::SDLWindow() : Window(INPUT_SDL), m_window(nullptr), m_context(), m_closeRequested(false)
     {
     }
 
     SDLWindow::~SDLWindow()
     {
     }
+
+    void SDLWindow::initialize(cString title, ui16f width, ui16f height, bool fullscreen, bool resizable, const VSync &vsync)
+    {
+        SDL_DisplayMode mode;
+        SDL_GetCurrentDisplayMode(0, &mode);
+
+        if (width == WINDOW_MONITOR_RESOLUTION)
+            width = mode.w;
+    }
 }
-#endif
